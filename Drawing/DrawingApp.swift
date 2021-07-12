57b2970dcfd468da36761bb1d0ac0ac2705911f9
Sergio Sepulveda <sergiosepulveda09@live.com>
2021-07-12 11:37:24 -0700

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Sergio Sepulveda on 2021-07-12.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
